54eb1943a58a4fc8825065a5626879c0fe1a4ad5
shalvah <diakon.ng@gmail.com>
2018-10-12 20:45:38 +0100

    Add scheduler stub and errors

---
+++ src/errors.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class TwitterErrorResponse extends Error {
+    constructor(endpoint, errors) {
+        super('Error from Twitter API call');
+        this.name = 'TwitterErrorResponse';
+        this.errors = errors;
+        this.endpoint = endpoint;
+    }
+}
+
+module.exports = { TwitterErrorResponse };
---
+++ src/scheduler.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    scheduleTweets: (tweets) => {
+        return tweets;
+    }
+};
